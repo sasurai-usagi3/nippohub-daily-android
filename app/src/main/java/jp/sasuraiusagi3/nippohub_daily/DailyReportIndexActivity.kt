008b40ef92d0d4a8efbeb048d50941c5a8a841e0
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-04-22 07:45:23 +0900

    first commit

---
+++ app/src/main/java/jp/sasuraiusagi3/nippohub_daily/DailyReportIndexActivity.kt
@@ -0,0 +1,12 @@
+package jp.sasuraiusagi3.nippohub_daily
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DailyReportIndexActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daily_report_index)
+    }
+}
