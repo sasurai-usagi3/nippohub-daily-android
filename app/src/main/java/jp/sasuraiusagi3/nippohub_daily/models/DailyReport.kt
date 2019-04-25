38738913225adc6467f52c0a8bf35151ba4ab792
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-04-26 07:54:36 +0900

    add: DailyReportモデル

---
+++ app/src/main/java/jp/sasuraiusagi3/nippohub_daily/models/DailyReport.kt
@@ -0,0 +1,14 @@
+package jp.sasuraiusagi3.nippohub_daily.models
+
+import java.time.LocalDate
+
+/**
+ * Created by sasurai-usagi3 on 2019/04/26.
+ */
+
+data class DailyReport(
+        val id: String,
+        val date: LocalDate,
+        val title: String,
+        val content: String
+)
