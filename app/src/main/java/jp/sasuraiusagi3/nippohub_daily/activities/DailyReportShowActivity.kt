9c5416b476f4467e9cd96fef63475d0d955a6d25
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-05-03 07:30:14 +0900

    add: Activity追加

---
+++ app/src/main/java/jp/sasuraiusagi3/nippohub_daily/activities/DailyReportShowActivity.kt
@@ -0,0 +1,13 @@
+package jp.sasuraiusagi3.nippohub_daily.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import jp.sasuraiusagi3.nippohub_daily.R
+
+class DailyReportShowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daily_report_show)
+    }
+}
