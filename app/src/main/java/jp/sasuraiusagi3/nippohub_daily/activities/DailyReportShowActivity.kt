ba526c33b7f7217861ed1c5931746bf93d1aadf5
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-05-03 09:33:49 +0900

    add: 表示できるようにした

--- app/src/main/java/jp/sasuraiusagi3/nippohub_daily/activities/DailyReportShowActivity.kt
+++ app/src/main/java/jp/sasuraiusagi3/nippohub_daily/activities/DailyReportShowActivity.kt
@@ -2,6 +2,8 @@ package jp.sasuraiusagi3.nippohub_daily.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import jp.sasuraiusagi3.nippohub_daily.R
 import jp.sasuraiusagi3.nippohub_daily.models.DailyReport
 
@@ -15,9 +17,26 @@ class DailyReportShowActivity : AppCompatActivity() {
         setContentView(R.layout.activity_daily_report_show)
 
         val dailyReport = this.intent.getSerializableExtra(DAILY_REPORT) as DailyReport
+        val webview = findViewById<WebView>(R.id.dailyReportShowWebView)
 
-        println("------------")
-        println(dailyReport)
-        println("------------")
+        webview.webViewClient = WebClientForDailyReport(dailyReport)
+        webview.settings.javaScriptEnabled = true
+        webview.loadUrl("file:///android_asset/html/daily_report_show.html")
+    }
+
+    private class WebClientForDailyReport(private val dailyReport: DailyReport): WebViewClient() {
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+
+            view ?: return
+
+            val title = "${dailyReport.date.toString()} ${dailyReport.title}".replace("'", "\\'")
+            val content = dailyReport.content
+                    .replace("\\", "\\\\")
+                    .replace("\n", "\\n")
+                    .replace("\"", "\\\"")
+
+            view.evaluateJavascript("rendering('$title', \"$content\")", {println("--------");println(it);println("--------");})
+        }
     }
 }
