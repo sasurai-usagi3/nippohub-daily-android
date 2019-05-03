d112c0c45a9df8ecb9137c0221dcccfec3fd9769
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-05-04 08:12:33 +0900

    add: 編集

--- app/src/main/java/jp/sasuraiusagi3/nippohub_daily/activities/DailyReportShowActivity.kt
+++ app/src/main/java/jp/sasuraiusagi3/nippohub_daily/activities/DailyReportShowActivity.kt
@@ -1,9 +1,11 @@
 package jp.sasuraiusagi3.nippohub_daily.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
 import jp.sasuraiusagi3.nippohub_daily.R
 import jp.sasuraiusagi3.nippohub_daily.models.DailyReport
 
@@ -18,10 +20,19 @@ class DailyReportShowActivity : AppCompatActivity() {
 
         val dailyReport = this.intent.getSerializableExtra(DAILY_REPORT) as DailyReport
         val webview = findViewById<WebView>(R.id.dailyReportShowWebView)
+        val btnToEdit = findViewById<Button>(R.id.dailyReportShowBtnToEdit)
 
         webview.webViewClient = WebClientForDailyReport(dailyReport)
         webview.settings.javaScriptEnabled = true
         webview.loadUrl("file:///android_asset/html/daily_report_show.html")
+
+        btnToEdit.setOnClickListener {
+            val intent = Intent(this, EditDailyReportActivity::class.java)
+
+            intent.putExtra(EditDailyReportActivity.DAILY_REPORT, dailyReport)
+
+            startActivity(intent)
+        }
     }
 
     private class WebClientForDailyReport(private val dailyReport: DailyReport): WebViewClient() {
---
+++ app/src/main/java/jp/sasuraiusagi3/nippohub_daily/activities/EditDailyReportActivity.kt
@@ -0,0 +1,51 @@
+package jp.sasuraiusagi3.nippohub_daily.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.EditText
+import com.google.firebase.database.FirebaseDatabase
+import jp.sasuraiusagi3.nippohub_daily.R
+import jp.sasuraiusagi3.nippohub_daily.models.DailyReport
+import jp.sasuraiusagi3.nippohub_daily.utils.AccountManager
+import java.time.LocalDate
+
+class EditDailyReportActivity : AppCompatActivity() {
+    companion object {
+        const val DAILY_REPORT = "dailyReport"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_daily_report)
+
+        val currentUser = AccountManager.currentUser ?: return
+        val dailyReport = this.intent.getSerializableExtra(DAILY_REPORT) as DailyReport
+        val formDate = findViewById<DatePicker>(R.id.editDailyReportFormDate)
+        val formTitle = findViewById<EditText>(R.id.editDailyReportFormTitle)
+        val formContent = findViewById<EditText>(R.id.editDailyReportFormContent)
+        val btnToSubmit = findViewById<Button>(R.id.editDailyReportBtnSubmit)
+        val database = FirebaseDatabase.getInstance().getReference("/users/${currentUser.uid}/daily_reports/${dailyReport.id}")
+
+        formDate.updateDate(dailyReport.date.year, dailyReport.date.monthValue - 1, dailyReport.date.dayOfMonth)
+        formTitle.setText(dailyReport.title)
+        formContent.setText(dailyReport.content)
+
+        btnToSubmit.setOnClickListener {
+            val date = LocalDate.of(formDate.year, formDate.month + 1, formDate.dayOfMonth)
+
+            database.setValue(
+                    mapOf(
+                            "date" to date.toString(),
+                            "title" to formTitle.text.toString(),
+                            "content" to formContent.text.toString(),
+                            "createdAt" to System.currentTimeMillis()
+                    )
+            )
+
+            finish()
+        }
+    }
+}
