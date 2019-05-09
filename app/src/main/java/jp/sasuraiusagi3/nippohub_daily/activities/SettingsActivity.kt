16bf0871ba764779d9d097740cb10ce72e260fcf
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-05-10 07:40:28 +0900

    add: 設定画面

---
+++ app/src/main/java/jp/sasuraiusagi3/nippohub_daily/activities/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package jp.sasuraiusagi3.nippohub_daily.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import jp.sasuraiusagi3.nippohub_daily.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
