1141d6098d063314c8b771630ae3f0efda72117f
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-07-18 07:29:56 +0900

    add: fragment作成

---
+++ app/src/main/java/jp/sasuraiusagi3/nippohub_daily/activities/DailyReportListFragment.kt
@@ -0,0 +1,50 @@
+package jp.sasuraiusagi3.nippohub_daily.activities
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ListView
+import androidx.fragment.app.Fragment
+import jp.sasuraiusagi3.nippohub_daily.R
+import jp.sasuraiusagi3.nippohub_daily.adapters.DailyReportListAdapter
+import jp.sasuraiusagi3.nippohub_daily.models.DailyReport
+import jp.sasuraiusagi3.nippohub_daily.repositories.DailyReportRepository
+import jp.sasuraiusagi3.nippohub_daily.repositories.UserRepository
+
+class DailyReportListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        return inflater.inflate(R.layout.fragment_daily_report_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val currentUser = UserRepository.currentUser ?: return
+        val adapter = DailyReportListAdapter(context!!)
+
+        view.findViewById<ListView>(R.id.daily_report_list_table_view).also {
+            it.adapter = adapter
+            it.onItemClickListener = DailyReportListClickListener(context!!)
+        }
+
+        DailyReportRepository.fetch(currentUser) {
+            adapter.dailyReports = it
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    private class DailyReportListClickListener(private val context: Context) : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            val dailyReport = parent?.adapter?.getItem(position) as DailyReport
+            val intent = DailyReportShowActivity.build(context, dailyReport)
+
+            context.startActivity(intent)
+        }
+    }
+}
