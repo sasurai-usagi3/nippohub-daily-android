61b958308e5d93abde4b9b6b9a005ecdb674dbcd
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-08-18 07:53:28 +0900

    add: DailyReportShareUseCase

--- app/src/main/java/jp/sasuraiusagi3/nippohub_daily/use_cases/DailyReportShareUseCase.kt
+++ app/src/main/java/jp/sasuraiusagi3/nippohub_daily/use_cases/DailyReportShareUseCase.kt
@@ -1,11 +1,30 @@
-package jp.sasuraiusagi3.nippohub_daily.usecases
+package jp.sasuraiusagi3.nippohub_daily.use_cases
 
 import jp.sasuraiusagi3.nippohub_daily.models.DailyReport
 import jp.sasuraiusagi3.nippohub_daily.models.User
+import jp.sasuraiusagi3.nippohub_daily.repositories.AccessKeyRepository
 import jp.sasuraiusagi3.nippohub_daily.repositories.DailyReportRepository
 
 object DailyReportShareUseCase {
+    /**
+     * アクセスキーを発行する
+     *
+     * @param user
+     * @param dailyReport
+     */
     fun share(user: User, dailyReport: DailyReport) {
+        val accessKey = AccessKeyRepository.create(user, dailyReport)
+        DailyReportRepository.updateAccessKey(user, dailyReport, accessKey.id)
+    }
 
+    /**
+     * アクセスキーを削除する
+     *
+     * @param user
+     * @param dailyReport
+     */
+    fun stopSharing(user: User, dailyReport: DailyReport) {
+        AccessKeyRepository.delete(user, dailyReport)
+        DailyReportRepository.updateAccessKey(user, dailyReport, null)
     }
 }
