0c4a2a238c15994fe6a0ac2280dce6a53eb16af8
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-08-18 07:49:48 +0900

    add: AccessKey

---
+++ app/src/main/java/jp/sasuraiusagi3/nippohub_daily/use_cases/DailyReportShareUseCase.kt
@@ -0,0 +1,11 @@
+package jp.sasuraiusagi3.nippohub_daily.usecases
+
+import jp.sasuraiusagi3.nippohub_daily.models.DailyReport
+import jp.sasuraiusagi3.nippohub_daily.models.User
+import jp.sasuraiusagi3.nippohub_daily.repositories.DailyReportRepository
+
+object DailyReportShareUseCase {
+    fun share(user: User, dailyReport: DailyReport) {
+
+    }
+}
